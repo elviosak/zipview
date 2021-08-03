7f88321506bf38d6ed4f49f406fe068a99091bfe
slidinghotdog <33790211+slidinghotdog@users.noreply.github.com>
2021-08-02 21:40:43 -0300

    1

--- delegate.cpp
+++ delegate.cpp
@@ -3,11 +3,7 @@
 void Delegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
     painter->save();
-//    auto rect = QRect(option.rect);
     auto pix = index.data(Qt::UserRole + 1).value<QPixmap>();
-//    int pixX = rect.x() + 2;
-//    int pixY = rect.y() + 2;
-//    QRect pixRect(pixX, pixY, TWIDTH, THEIGHT);
     QApplication::style()->drawPrimitive(QStyle::PE_PanelItemViewItem, &option, painter);
     QApplication::style()->drawPrimitive(QStyle::PE_PanelItemViewRow, &option, painter);
     QApplication::style()->drawItemPixmap(
